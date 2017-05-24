baa12fb06e206f9023570e96dd5c15fc3f96ef33
Tim van Dalen <Tim@timvdalen.nl>
2017-05-24 14:24:02 +0200

    Add demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import * as math from 'mathjs';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+	value = 0;
+
+	constructor() {
+		this.value = math.parse('2+2').compile().eval();
+	}
 }
